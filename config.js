06df9036430e78b1a52ee4c46968f5958e92274a
deniz <koreli_deniz@naver.com>
2026-02-27 19:09:59 +0900

    Update config.js

--- config.js
+++ config.js
@@ -8,13 +8,13 @@ module.exports = {
     authorImage: "https://placehold.co/1080x1080",
     socialLinks: {
         linkedin: "https://www.linkedin.com/in/johndoe",
-        behance: "https://www.behance.net/johndoe",
-        instagram: "https://www.instagram.com/johndoedesign"
+        behance: "https://www.behance.net/juyeong_an",
+        instagram: "https://www.instagram.com/korelideniz.kr"
     },
     heroTexts: [
+        "Brand Designer",
         "Graphic Designer",
-        "UI/UX Designer",
-        "Visual Artist"
+        "content creator"
     ],
     heroBio: "I'm a passionate designer with over 5 years of experience in creating user-centered digital experiences. My goal is to blend creativity with functionality to deliver impactful designs.",
     ctaButton: {
